e66b5b8ce82cf89da9a4446e2a898735d2025e23
rts-tamak <RiteshTamak@outlook.com>
2024-07-23 13:29:46 +0530

    duplicate no

--- Array/find the duplicate no.cpp
+++ Array/find the duplicate no.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        	    int ans = 0;
-    
-    //XOR ing all array elements
-    for(int i = 0; i<nums.size(); i++ ) {
-    	ans = ans^nums[i];
-    }
-	
-    //XOR [1, n-1]
-    for(int i = 1; i<nums.size();i++ ) {
-    	ans = ans^i;
-    }
-    return ans;
+    int findDuplicate(vector<int>& nums) 
+    {
+                sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return 0;
     }
 };
