9b82eb51de1b747374dc30eb36636f06d00b1096
rts-tamak <RiteshTamak@outlook.com>
2024-08-10 11:20:48 +0530

    reverse pairs

---
+++ Array/reverse pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        multiset<int> s;
+        int count = 0;
+
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            auto it = s.lower_bound((nums[i] % 2 == 0) ? (nums[i] / 2) : (nums[i] / 2 + 1));
+            count += distance(s.begin(), it); 
+            s.insert(nums[i]);  
+        }
+
+        return count;
+    }
+};
